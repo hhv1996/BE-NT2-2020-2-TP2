e8a300feaba7938c21594471a4eee3b6e4a6ec82
unknown <hhv1996@gmail.com>
2020-09-27 23:23:03 -0300

    retrabajo-V2.0

--- App.js
+++ App.js
@@ -4,58 +4,89 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import {vibrate} from './utils';
 import Reloj from "./utils/reloj";
 
-export default function App() {
-let minTrab= 0;
-let minDesc=5;
-let segg=60; 
-let textTipo="Tiempo de Trabajar";
-let esDescanso=false;
+//const [min,descontarMin]=useState(0)
+const minutos =sMin=>{
+  const [min,descontarMin]=useState(sMin);
+  const desMin =()=>{
+      if(min!=0)
+        descontarMin(min-1);
+    };
+  const setMinutos = setMin=>{
+    descontarMin(min-min+setMin);
+  };
 
-const [min,descontarMin]=useState(minTrab);
-const [seg,descontarSegundos]=useState(segg);
+  return {min,desMin,setMinutos};
+};
 
-
-
-useEffect(()=>{
-  if(seg!=60 && seg>-1)
-  {
-    setTimeout(()=>{
+const segundos =()=>{
+  const [seg,descontarSegundos]=useState(60);
+  const desSeg =()=>{
+    if(seg==0)
+      descontarSegundos(seg+59);
+    else
       descontarSegundos(seg-1);
-    },100); 
-  }else{
-    if (seg==-1 && min > 0){
-      descontarSegundos(seg + 60);
-      descontarMin(min-1);
+  }
+  return {seg,desSeg};
+};
+
+const reloj =minReloj=>{
+  const {seg,desSeg}= segundos();
+  const {min,desMin,setMinutos}= minutos(minReloj-1);
+
+  const funcionar = ()=>{
+    if(seg==0){
+      desMin();
+    }
+    desSeg();
+  };
+  return {seg,min,funcionar,setMinutos};
+};
+
+let esDescanso=true;
+let textTitulo = "Hora de trabajar! :(";
+const relojPomodoro = (minDes,minTrab)=>{
+  const {seg,min,funcionar,setMinutos}=reloj(minTrab);
+
+  const comenzar = ()=>{
+    console.log(min,seg,esDescanso);
+    funcionar();
+    if(min==0&&seg==1&&esDescanso){
+      textTitulo = "Hora de descansar! :D";
+      setMinutos(minDes-1);
+      esDescanso=false;
+      vibrate();
     }else{
-      if(seg==-1 && min <= 0)
-      {
-        descontarSegundos(seg + 61);
-        descontarMin(min + minDesc-1);
-        Alert.alert(
-          "Tiempo terminado",
-          ":)",
-          [
-            {
-              text: "Ok",
-              onPress: () => console.log("holoa")
-            }
-          ]); 
+      if(min==0&&seg==1&&esDescanso==false){
+        textTitulo = "Hora de trabajar! :(";
+        setMinutos(minTrab-1);
+        esDescanso=true;
+        vibrate();
       }
     }
-  }
-},[seg])
+  };
+  return {seg,min,comenzar};
+};
 
-function empezar(){
-  descontarSegundos(seg-1);
-}
+
+export default function App() {
+const {seg,min,comenzar}=relojPomodoro(1,2);
+useEffect(()=>{
+  if(seg!=60){
+    setTimeout(()=>{
+      comenzar();
+    },1000); 
+  }
+},[seg]);
   return (
     <View style={styles.container}>
-      <Reloj textTipo={textTipo} seg={seg} min={min}></Reloj>
+      <Reloj textTipo={textTitulo} seg={seg} min={min}></Reloj>
       <Button 
+        style={styles.boton}
         title="Empezar"
-        onPress={empezar}
+        onPress={comenzar}
       />
       <StatusBar style="auto" />
+      <Text style={styles.texto}>-----------------</Text>
     </View>
   );
 
@@ -64,8 +95,15 @@ function empezar(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boton:{
+    fontSize:60
+  },
+  texto:{
+    color:'white',
+    fontSize:40
+  }
 });
--- utils/reloj.js
+++ utils/reloj.js
@@ -5,11 +5,21 @@ export default({min,seg,textTipo})=>{
 
 
     return(
-        <View>
-            <Text>{textTipo}</Text>
-            <Text>Tiempo restante:</Text>
-            <Text>{min}:{seg}</Text>            
+        <View style={styles.reloj}>
+            <Text style={styles.textoTitulo}>{textTipo}</Text>
+            <Text style={styles.texto}>Tiempo restante:</Text>
+            <Text style={styles.textoReloj}>{min}:{seg}</Text>            
         </View>
     );  
 }
   
+const styles = StyleSheet.create({
+    textoTitulo:{
+        color:'white',
+        fontSize:30
+    },
+    textoReloj:{
+        color:'white',
+        fontSize:150
+    }
+  });
